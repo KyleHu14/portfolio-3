9485b5e24dff8bf84054eb1f1d4415e4450d2c21
KyleHu14 <lloydpearce4@gmail.com>
2023-06-06 23:57:34 -0700

    Working on mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,38 +1,65 @@
+// React
+import { useState } from "react";
+
 // Next Links
 import Link from "next/link";
+import Image from "next/image";
 
 // CSS
 import NavStyles from "@/styles/components/Navbar.module.css";
 
 const Navbar = () => {
-  return (
-    <nav className={NavStyles.navContainer}>
-        <div className={NavStyles.nav}>
-            <Link
-                className={`${NavStyles.linkControl} ${NavStyles.homeLink}`}
-                href="/">
-                Welcome!
-            </Link>
-            <div className={NavStyles.navRight}>
-                <Link
-                    className={`${NavStyles.linkControl} ${NavStyles.rightLink}`}
-                    href="/">
-                    Home
-                </Link>
-                <Link
-                    className={`${NavStyles.linkControl} ${NavStyles.rightLink}`}
-                    href="/projects">
-                    Projects
-                </Link>
-                <Link
-                    className={`${NavStyles.linkControl} ${NavStyles.rightLink}`}
-                    href="/">
-                    Blog
-                </Link>
-            </div>
-        </div>
-    </nav>
-  )
-}
+    const [showMobileNavbar, setMobileNavBar] = useState(false);
 
-export default Navbar
+    const handleMenuClick = () => {
+		setMobileNavBar(!showMobileNavbar);
+    }
+
+	return (
+		<nav className={NavStyles.navContainer}>
+            {/* Main Navbar that laptop devices see */}
+			<div className={NavStyles.mainNavBar}>
+                <Link
+                    className={`${NavStyles.linkControl} ${NavStyles.homeLink}`}
+                    href="/">
+                    Welcome!
+                </Link>
+				<div className={NavStyles.navRight}>
+					<Link
+						className={`${NavStyles.linkControl} ${NavStyles.rightLink}`}
+						href="/">
+						Home
+					</Link>
+					<Link
+						className={`${NavStyles.linkControl} ${NavStyles.rightLink}`}
+						href="/projects">
+						Projects
+					</Link>
+					<Link
+						className={`${NavStyles.linkControl} ${NavStyles.rightLink}`}
+						href="/">
+						Blog
+					</Link>
+				</div>
+			</div>
+
+            {/* Mobile Navbar */}
+            <div className={NavStyles.mobileNavbar}>
+                <Image className={NavStyles.MenuIcon} src="/icons/Bars3.svg" width={50} height={50} alt="" onClick={handleMenuClick}/>
+				{ showMobileNavbar ? 
+					(
+						<div className={NavStyles.mobileDropDown}>
+							<div className={NavStyles.mobileNavLink}>Home</div>
+							<div className={NavStyles.mobileNavLink}>Projects</div>
+							<div className={NavStyles.mobileNavLink}>Blog</div>
+						</div>
+					) : null
+				}
+                
+            </div>
+            
+		</nav>
+	);
+};
+
+export default Navbar;
